21fec066069de09c30629e153febfa81a6d339e5
je-rry <9.uighjk@gmail.com>
2025-10-01 01:51:24 +0900

    day1

---
+++ cpp-guide/01_HelloWorld/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>     // 콘솔 입출력 = stream
+
+using namespace std;    // 네임스페이스 std::cout
+
+int main() {    // entry point : 프로그램의 시작점, 운영체제가 가장 먼저 호출하는 함수
+
+    cout << "Hello, World!" << endl;
+    // printf("Hello, World!\n");   // 자료형 신경써야함
+
+    return 0;
+}
+
